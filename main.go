cdc84959080170baca5f0fc763c5557ac45e7ab7
emre.savci <freelancemuhendis@gmail.com>
2021-04-04 17:35:57 +0300

    add subscription examples

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	dapr "github.com/dapr/go-sdk/client"
 	"io/ioutil"
 	"net/http"
@@ -18,6 +19,33 @@ func main() {
 	http.HandleFunc("/publish", func(writer http.ResponseWriter, request *http.Request) {
 		err = client.PublishEvent(context.Background(), "rabbitmq-pubsub", "test-topic", []byte("hello dapr"))
 		respond(nil, err, writer)
+	})
+
+	http.HandleFunc("/subscription", func(writer http.ResponseWriter, request *http.Request) {
+		byteBody,_:= ioutil.ReadAll(request.Body)
+		fmt.Println(string(byteBody))
+		writer.Header().Set("content-type","application/json")
+		respond([]byte("{'success':True}"), nil, writer)
+	})
+
+	http.HandleFunc("/dapr/subscribe", func(writer http.ResponseWriter, request *http.Request) {
+		subscriptions := []interface{}{
+			map[string]interface{}{
+				"pubsubname": "rabbitmq-pubsub",
+				"topic": "test-topic",
+				"route": "/subscription",
+			},
+		}
+
+		writer.Header().Add("content-type", "application/json")
+
+		subs, err :=json.Marshal(subscriptions)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		writer.Write(subs)
 	})
 
 	http.HandleFunc("/invoke-service", func(writer http.ResponseWriter, request *http.Request) {
@@ -63,6 +91,10 @@ func main() {
 	http.HandleFunc("/delete-state", func(writer http.ResponseWriter, request *http.Request) {
 		err := client.DeleteState(context.Background(), "my-redis","my-key")
 		respond(nil, err, writer)
+	})
+
+	http.HandleFunc("/forbidden", func(writer http.ResponseWriter, request *http.Request) {
+		respond([]byte("ok"), nil, writer)
 	})
 
 	http.ListenAndServe(":8080", nil)
